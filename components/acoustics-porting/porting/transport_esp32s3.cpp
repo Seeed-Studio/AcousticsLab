b4b37b8dd7d55bb76fe9a83d0a8d91b62c414d15
iChizer0 <62390647+iChizer0@users.noreply.github.com>
2026-01-28 17:33:26 +0800

    feat: add uart 1

--- components/acoustics-porting/porting/transport_esp32s3.cpp
+++ components/acoustics-porting/porting/transport_esp32s3.cpp
@@ -1,5 +1,6 @@
 #include "hal/transport.hpp"
 
+#include "transport/uart_1.hpp"
 #include "transport/uart_console.hpp"
 
 namespace bridge {
@@ -7,6 +8,7 @@ namespace bridge {
 void __REGISTER_TRANSPORTS__()
 {
     [[maybe_unused]] static porting::TransportUARTConsole transport_uart_console;
+    [[maybe_unused]] static porting::TransportUART1 transport_uart_1;
 }
 
 } // namespace bridge
